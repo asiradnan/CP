73256c761035b2c893b8c80ee3ccb24a8b677224
Asir Adnan <106885381+asiradnan@users.noreply.github.com>
2024-02-11 12:41:08 +0600

    Create Set.cpp

---
+++ STL/Set.cpp
@@ -0,0 +1,30 @@
+//Contructors:
+set<int> first;  // empty set of ints
+int myints[]= {10,20,30,40,50};
+set<int> second (myints,myints+5); // range
+set<int> third (second); // a copy of second
+
+//Insertion:
+st.insert(5); //O(logn)
+
+//Access:
+st.find(5) //O(logn) returns the iterator 
+auto it = st.lower_bound (30); //O(logn)  //30 thakle 30, na thakle immediate boro
+auto it = st.upper_bound (60); //O(logn)  //thakuk na thakuk immediate boro
+for (auto it = st.begin(); it != st.end(); it++) cout << ' ' << *it;
+for (auto it = st.rbegin(); it!= st.rend(); rit++) cout << ' ' << *it;
+
+//Size:
+st.empty(); //O(1) //returns true if the size is 0 //declared with a size er khetre false return korbe
+st.size(); //O(1) //returns the first element
+
+//Deletion:
+st.erase (it); //O(1)
+st.erase (40); //O(logn)
+auto it = myset.find (60);
+st.erase (it, st.end()); //O(n)
+st.clear(); //O(n) //deletes the set and makes size to 0
+
+//Sorting
+sort(st.begin(),st.end()); //ascending order
+sort(st.rbegin(),st.rend()); //descending order
