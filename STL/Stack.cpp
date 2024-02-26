17845988df4f02f632875a979b185614916278ad
Asir Adnan <106885381+asiradnan@users.noreply.github.com>
2024-02-26 09:39:38 +0600

    Create Stack.cpp

---
+++ STL/Stack.cpp
@@ -0,0 +1,15 @@
+//Contructors:                               
+stack<int> sk; // empty stack for int
+
+//Insertion:
+sk.push(5); //O(1)
+
+//Access:
+sk.top() //O(1) returns the first element
+
+//Size:
+sk.empty(); //O(1) //returns true if the size is 0 //declared with a size er khetre false return korbe
+sk.size(); //O(1) //returns the size
+
+//Deletion:
+sk.pop(); //O(1) //deletes last element
